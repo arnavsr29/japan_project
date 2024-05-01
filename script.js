be1f32f0069f736b3c8cb28152a84f3caf0d015d
Arnav Srivastava <Arnavsrivastava2909@gmail.com>
2024-05-02 03:22:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+body {
+    background-image: url("https://i.pinimg.com/564x/bf/d1/12/bfd11294cce218c7d76d20b0337e9ce1.jpg");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-attachment: fixed;
+    background-color: beige;
+    text-align: center;
+}
+.icon-heading {
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+    background-color: lightcoral;
+    color:ghostwhite;
+}
+ div h1 {
+    font-family:fantasy Papyrus;
+}
+.last-section {
+    text-align:center;
+    font-family: fantasy Papyrus;
+    color: ghostwhite;
+}
+ div {
+    text-align:center;
+    text-decoration:solid;
+    font-size:x-large;  
+}
+.myModal {
+    position: fixed;  
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5); 
+    display: flex;  
+    justify-content: center;  
+    align-items: center;  
+    z-index: 1; 
+    overflow: auto;
+}
+.modal {
+    display:none;
+}
+.modal-content {
+    background-color: white;
+    margin: 5px auto;
+    padding: 100px;
+    border: 1px solid;
+    border-color: dimgray;
+    width: 500px;
+    text-align: center;
+}
+.button-group .open-button {
+    padding: 20px 20px; 
+    border: 1px solid black;  
+    text-align: center; 
+    background-color: aliceblue;
+}
